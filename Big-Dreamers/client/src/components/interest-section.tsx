fae1a90ab68c685e8bb5c9032572bb79be9ec20c
rodriknd <rp752989@gmail.com>
2025-12-12 18:39:27 -0400

    chore:Putting background color white in the tag HTML section

--- Big-Dreamers/client/src/components/interest-section.tsx
+++ Big-Dreamers/client/src/components/interest-section.tsx
@@ -59,7 +59,7 @@ export default function InterestSection() {
     ];
 
     return (
-        <section id="investment" className="py-20">
+        <section id="investment" className="py-20 bg-white">
             <div className="container mx-auto px-4">
                 <div className="text-center mb-16">
                     <h2
